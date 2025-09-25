b5dfdcfede25f2ac7bc58a55e34edb94242808ef
Dani S. <daniela.schomaker@posteo.de>
2025-09-25 21:29:54 +0200

    feat: add renderPokemonGallery.js

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
+// let pokemonName = [];
+
+let pokemonData = [];
+
 function init() {
-  showPokemon();
+  renderPokemonGallery();
+  // renderPokemonImg();
 }
 
-async function showPokemon() {
-  let response = await fetch("https://pokeapi.co/api/v2/pokemon/1"); // #TODO: mehrere holen // liefert ein OBJEKT
-  let responseAsJson = await response.json();
+async function renderPokemonGallery() {
+  let pokemonList = await fetch(
+    "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0"
+  ); // liefert ein OBJEKT
+  let pokemonListAsJson = await pokemonList.json();
 
-  console.log(responseAsJson.forms[0].name);
+  // console.log(responseAsJson);
+
+
+  for (let i = 0; i < pokemonListAsJson.results.length; i++) {
+    let pokemonDetails = await fetch (pokemonListAsJson.results[i].url);
+    let pokemonDetailsAsJson = await pokemonDetails.json();
+
+    pokemonName.push(pokemonListAsJson.results[i].name);
+    document.getElementById("pokemon_gallery").innerHTML +=
+      getPokemonGalleryTemplate(i);
+  }
+  console.log(pokemonName);
 }
+
+
+
+
+// TIPP FREDDY:
+// async function showPokemon() {
+//   for (let index = 0; index < 20; index++) {
+//     let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + index); // #TODO: mehrere holen // liefert ein OBJEKT
+//     let responseAsJson = await response.json();
+
+//     // in Array pushen (global definieren)
+//   }
+
+//   console.log(responseAsJson.forms[0].name);
+// }
